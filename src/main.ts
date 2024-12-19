98be46fe6d2457f82725db9e7f62b8f229435d86
Laurentius <laurentiuskevin44@gmail.com>
2024-12-20 02:36:06 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://hyperhire-test-menu-frontend-38blwhj2a.vercel.app/',
+    origin: 'https://hyperhire-test-menu-frontend-38blwhj2a.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
